d7536f2b863c50db6a9d0be3d667065431804158
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-08-12 08:30:38 +0300

    Updates

---
+++ precache-manifest.3ef23a925c094f288fc81434e3d920a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81eb2a72c681a9809d869ad3f00d1ed4",
+    "url": "/gitapp-new-osher/index.html"
+  },
+  {
+    "revision": "5915a9207ce4f8dfd568",
+    "url": "/gitapp-new-osher/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7779a8eca56fad260caa",
+    "url": "/gitapp-new-osher/static/js/2.c10c8157.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitapp-new-osher/static/js/2.c10c8157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5915a9207ce4f8dfd568",
+    "url": "/gitapp-new-osher/static/js/main.93d0d09e.chunk.js"
+  },
+  {
+    "revision": "f63c4484fdf038280179",
+    "url": "/gitapp-new-osher/static/js/runtime-main.d9964275.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitapp-new-osher/static/media/logo.5d5d9eef.svg"
+  }
+]);
